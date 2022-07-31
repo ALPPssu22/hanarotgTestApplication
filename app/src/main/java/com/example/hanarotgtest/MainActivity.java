ec64f2ac34c0f36ae89cd3bf62e783b519b18517
Taegyeong Lee <hanarotg@naver.com>
2022-07-31 18:42:16 +0900

    tmp
    tmp

--- app/src/main/java/com/example/hanarotgtest/MainActivity.java
+++ app/src/main/java/com/example/hanarotgtest/MainActivity.java
@@ -1,14 +1,85 @@
 package com.example.hanarotgtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void setAlarm(View view){
+        
+    }
+
+
+    public void onAlarm(View view) {
+
+        /*
+        // ringtone
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE );
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "notiff")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("알림 타이틀")
+                .setContentText("초비사사사사사사아아아아앙")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        builder.setSound(alarmSound);
+
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "알림 타이틀";
+            String description = "초비사사사사사사아아아아앙";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("notiff", name, importance);
+            channel.setDescription(description);
+
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // 지속적인 알림 발생 유지
+        Notification mNotification = builder.build();
+        mNotification.flags |= Notification.FLAG_INSISTENT;
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(22, mNotification);
+
+        */
+
+        Log.i("mytag","앙");
+    }
+
 }
